ac2e26865df096563cd7277c343270aa518e6e8c
rayssaetec <rayssa.gomes19@etec.sp.gov.br>
2024-09-03 14:28:01 -0300

    Aula 03

---
+++ aulas/aula03/calculo.js
@@ -0,0 +1,7 @@
+function somar () {
+    var x = 2
+    var y = 2
+    let res = x+y
+    document.getElementById("soma").innerHTML=res
+    document.getElementById("res-soma").innerHTML="calculado ✔"
+}
